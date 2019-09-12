ee4eb1761d596a168f9bce4403e4e7132bdba62e
kajanishams <44614706+kajanishams@users.noreply.github.com>
2019-09-12 12:10:51 -0400

    Add files via upload

---
+++ Session3.java
@@ -0,0 +1,21 @@
+
+public class Session3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String name = null;
+		if ("Bob".equals(name)) {
+			System.out.println( "Bob equals name");
+			
+		}
+		if (name.equals("Bob")) {
+			System.out.println("Name equals Bob");
+		}
+		if ("Bob"== name) {
+			System.out.println ("Bob is name");
+		}
+	}
+
+}
