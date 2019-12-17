b3e11c3be4b193464cfde0a2ba6a0d9cf133613a
kajanishams <44614706+kajanishams@users.noreply.github.com>
2019-12-17 13:08:00 -0500

    Update ServerThread.java

--- Final Project/ServerThread.java
+++ Final Project/ServerThread.java
@@ -9,13 +9,15 @@ import java.net.Socket;
 public class ServerThread extends Thread{
 	private Socket client;//server's reference to the connect client
 	private String clientName;//this connected client's name
-	private String key;
+	public String encryptionkey;
+	public String decryptionkey;
 	MyUI ui;
 	//Object streams that let us pass objects
 	private ObjectInputStream in;//from client
 	private ObjectOutputStream out;//to client
 	private boolean isRunning = false;//boolean to control termination
 	SampleSocketServer server;
+	SampleSocketClient cl;
 	public ServerThread(Socket myClient, String clientName, SampleSocketServer server) throws IOException {
 		this.client = myClient;
 		this.clientName = clientName;
@@ -58,16 +60,26 @@ public class ServerThread extends Thread{
 		System.out.println("Received: " + payload);
 		switch(payload.payloadType) {
 			case MESSAGE:
-                final String secretkey = "passwordpassword";
-                String encryptedString = SampleSocketServer.encrypt(payload.message, secretkey);
-                Payload toClient = new Payload(PayloadType.MESSAGE,clientName + ": " + encryptedString);
+               // final String secretkey = "passwordpassword";
+                String encryptedString = SampleSocketServer.encrypt(payload.message, encryptionkey);
+                Payload toClient = new Payload(PayloadType.MESSAGE, encryptedString);
                 System.out.println("Sending: " + toClient.toString());
-                server.broadcast(toClient);
+                server.broadcast(toClient,clientName);
                 
-			break;
+                break; 
 			
 			case TEXT:
-				server.HandleText(clientName, payload.message);
+				//final String secret = "passwordpassword";
+				String encryptedString1 = SampleSocketServer.encrypt(payload.message, encryptionkey);
+                Payload toClient1 = new Payload(PayloadType.MESSAGE,clientName + ": " + encryptedString1);
+                System.out.println("Sending: " + toClient1.toString());
+                server.broadcast(toClient1);
+				
+				break;
+			case CONNECT:
+				String[] keys = payload.message.split(":");
+				encryptionkey = keys[0];
+				decryptionkey = keys[1];
 				break;
 			
 			case DISCONNECT:
@@ -94,6 +106,7 @@ public class ServerThread extends Thread{
 	}
 	public void send(Payload p) {
 		try {
+			System.out.print("Sending" + p.message);
 			out.writeObject(p);
 		} catch (IOException e) {
 			System.out.println("Error sending payload to client");
